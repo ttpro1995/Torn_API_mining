ebf87dd06feac5f2e42b70a1ab9bd842c10cc955
Thai Thien <ttpro1995@yahoo.com.vn>
2018-04-15 19:02:48 +0700

    helloworld with log4j slf4j

---
+++ torn/src/main/java/me/thaithien/torn/Meow.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.thaithien.torn;
+
+import java.io.IOException;
+import java.util.Collections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author thaithien
+ */
+public class Meow {
+
+
+
+    public static void main(String[] args)  {
+        Logger logger = LoggerFactory.getLogger(Meow.class);
+        logger.info("Hello World");
+    }
+
+}
