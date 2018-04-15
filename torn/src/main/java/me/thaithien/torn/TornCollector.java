e5e4ac8cf9b547545e88ecd55029f84fc28980d0
Thai Thien <tthien@apcs.vn>
2018-04-15 21:32:45 +0700

    TornCollector can make api request

---
+++ torn/src/main/java/me/thaithien/torn/TornCollector.java
@@ -0,0 +1,68 @@
+package me.thaithien.torn;
+
+
+
+
+
+import com.github.kevinsawicki.http.HttpRequest;
+
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+
+public class TornCollector {
+
+    /**
+     * environment variable name that contain torn api
+     * in Terminal
+     * Type:  export TORN_API=your_torn_api_key
+     */
+    public static final String ENV_TORN_API = "TORN_API";
+
+    private String userAgent = null;
+
+    public TornCollector(){}
+
+    public void setUserAgent(String userAgent){
+        this.userAgent = userAgent;
+    }
+
+    public String getUserAgent(){
+        return this.userAgent;
+    }
+
+    /**
+     * Collect summary information of user
+     * @param id
+     * @return json
+     */
+    public String collectUserByID(String id){
+        String torn_api_key = System.getenv(ENV_TORN_API);
+        // https://api.torn.com/user/2096019?key=your_api_key
+        String url = "https://api.torn.com/user";
+        String charset = java.nio.charset.StandardCharsets.UTF_8.name();
+        String result = null;
+        try {
+
+            String query = String.format("/user/%s?key=%s",
+                    URLEncoder.encode(id, charset),
+                    URLEncoder.encode(torn_api_key, charset));
+
+//            URLConnection connection = new URL(url + query).openConnection();
+//            connection.setRequestProperty("Accept-Charset", charset);
+//            InputStream response = connection.getInputStream();
+//            result = IOUtils.toString(response, StandardCharsets.UTF_8);
+
+            String response = HttpRequest.get(url + query).userAgent(this.userAgent).body();
+            result = response;
+
+        } catch (UnsupportedEncodingException e){
+            e.printStackTrace();
+            result = e.toString();
+        }
+
+        return result;
+    }
+
+}
